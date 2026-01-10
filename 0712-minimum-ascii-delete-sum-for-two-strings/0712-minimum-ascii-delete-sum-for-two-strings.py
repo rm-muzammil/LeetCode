d83d1d53d4f8a4c39b971ecfb4c2e219a314ca00
RM Muzammil <50529410+rm-muzammil@users.noreply.github.com>
2026-01-10 20:53:29 +0500

    Time: 307 ms (92.31%), Space: 13.5 MB (90.38%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minimumDeleteSum(self, s1, s2):
+        m, n = len(s1), len(s2)
+
+        # dp[i][j] = maximum ASCII sum of common subsequence
+        # between s1[0..i-1] and s2[0..j-1]
+        dp = [[0]*(n+1) for _ in range(m+1)]
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + ord(s1[i-1])
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        # Total ASCII of both strings
+        total = sum(ord(c) for c in s1) + sum(ord(c) for c in s2)
+
+        # dp[m][n] is max ASCII of common subsequence
+        return total - 2 * dp[m][n]
